c6392020e450d79b5600f1810316d3cda60c4332
JIELSAN <jesussjg811@gmail.com>
2024-08-13 14:19:54 -0600

    modificacion del nombre de la bd

--- php/conexion.php
+++ php/conexion.php
@@ -1,6 +1,6 @@
 <?php 
 
-    $conexion = new mysqli('localhost','root','','sistemaverificacion');
+    $conexion = new mysqli('localhost','root','','pagina_tdb');
     if($conexion-> connect_error){
         die('No se pudo conectar al servidor');
     }
