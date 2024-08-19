f304860d83919b9c682ad1d3c74392937a139365
Jesus Emmanuel San Juan Gomez <dsm21190086.jsanjuan@alumnos.utsv.edu.mx>
2024-08-19 16:33:52 -0600

    modificacion estrutura navbar

--- estructura/navbar.php
+++ estructura/navbar.php
@@ -1,30 +1,30 @@
 <nav class="navbar navbar-expand-lg border-bottom" class="text-bg-dark p-3">
   <div class="container-fluid">
-    <a class="navbar-brand" href="../../PaginaTheBlueDump/index.php">Inicio</a>
+    <a class="navbar-brand" href="../../PaginaTheBlueDump-main/index.php">Inicio</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="../../PaginaTheBlueDump/pages/controles.php">Controles</a>
+          <a class="nav-link active" aria-current="page" href="../../PaginaTheBlueDump-main/pages/controles.php">Controles</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" href="../../PaginaTheBlueDump/pages/novedades.php">Novedades</a>
+          <a class="nav-link active" href="../../PaginaTheBlueDump-main/pages/novedades.php">Novedades</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Elementos
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="../../PaginaTheBlueDump/pages/personajes.php">Personajes</a></li>
-            <li><a class="dropdown-item" href="../../PaginaTheBlueDump/pages/items.php">Items</a></li>
+            <li><a class="dropdown-item" href="../../PaginaTheBlueDump-main/pages/personajes.php">Personajes</a></li>
+            <li><a class="dropdown-item" href="../../PaginaTheBlueDump-main/pages/items.php">Items</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="../../PaginaTheBlueDump/pages/puntuacion.php">Puntuación</a></li>
+            <li><a class="dropdown-item" href="../../PaginaTheBlueDump-main/pages/puntuacion.php">Puntuación</a></li>
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" href="../../PaginaTheBlueDump/pages/contacto.php">Contactos</a>
+          <a class="nav-link active" href="../../PaginaTheBlueDump-main/pages/contacto.php">Contactos</a>
         </li>
       </ul>
       <form class="d-flex me-2" role="search">
@@ -32,8 +32,8 @@
         <button class="btn btn-outline-success " type="submit">Buscar</button>
       </form>
       <div class="btn-group mt-2 mb-2">
-        <a href="../../PaginaTheBlueDump/login.php" class="btn btn-primary">Sign In</a>
-        <a href="../../PaginaTheBlueDump/register.php" class="btn btn-primary">Sign Up</a>
+        <a href="../../PaginaTheBlueDump-main/login.php" class="btn btn-primary">Sign In</a>
+        <a href="../../PaginaTheBlueDump-main/register.php" class="btn btn-primary">Sign Up</a>
       </div>
     </div>
   </div>
