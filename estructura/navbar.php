27e6a0928c0963566f652036cb33baecd5f719c2
Jesus Emmanuel San Juan Gomez <dsm21190086.jsanjuan@alumnos.utsv.edu.mx>
2024-08-19 16:00:18 -0600

    actualizacion de links de carpetas

--- estructura/navbar.php
+++ estructura/navbar.php
@@ -7,24 +7,24 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="../../PaginaTheBlueDump/page/contacto.php">Controles</a>
+          <a class="nav-link active" aria-current="page" href="../../PaginaTheBlueDump/pages/contacto.php">Controles</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" href="../../PaginaTheBlueDump/page/novedades.php">Novedades</a>
+          <a class="nav-link active" href="../../PaginaTheBlueDump/pages/novedades.php">Novedades</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Elementos
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="../../PaginaTheBlueDump/page/personajes.php">Personajes</a></li>
-            <li><a class="dropdown-item" href="../../PaginaTheBlueDump/page/items.php">Items</a></li>
+            <li><a class="dropdown-item" href="../../PaginaTheBlueDump/pages/personajes.php">Personajes</a></li>
+            <li><a class="dropdown-item" href="../../PaginaTheBlueDump/pages/items.php">Items</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="../../PaginaTheBlueDump/page/puntuacion.php">Puntuación</a></li>
+            <li><a class="dropdown-item" href="../../PaginaTheBlueDump/pages/puntuacion.php">Puntuación</a></li>
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" href="../../PaginaTheBlueDump/page/contacto.php">Contactos</a>
+          <a class="nav-link active" href="../../PaginaTheBlueDump/pages/contacto.php">Contactos</a>
         </li>
       </ul>
       <form class="d-flex me-2" role="search">
